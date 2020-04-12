6bf1814bf8c13132a8ecc539d5f107ad9f24f500
Rodneystar <Rodneystar66@gmail.com>
2020-04-12 15:16:18 +0100

    second commit add dockerfile etc

--- src/main/java/com/example/jdog/distributedServer/MessageController.java
+++ src/main/java/com/example/jdog/distributedServer/MessageController.java
@@ -3,6 +3,7 @@ package com.example.jdog.distributedServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,12 @@ public class MessageController {
     @PostMapping(path="/message")
     public ResponseEntity<?> postMessage(@RequestBody DistMessage message) {
         logger.info("message from client: {}, {}", message.fromId, message.message);
+        return ResponseEntity.ok().build();
+    }
+
+    @GetMapping(path="/test")
+    public ResponseEntity<?> getTest() {
+        logger.info("request received");
         return ResponseEntity.ok().build();
     }
 
